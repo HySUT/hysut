b3a0719b0264546462361084feb33615de52c183
Mohammad Amin Tahavori <50955527+mohammadamint@users.noreply.github.com>
2022-05-04 15:43:07 +0200

    checking the consistency/overlapping of time period
    - added to time.py as check_time_period_overlaps
    - Checking if the given periods dont have any overlap
    - Catching the errors and returning the error
    - tests passed through test_preprocess.py

--- hysut/preprocess/time.py
+++ hysut/preprocess/time.py
@@ -1,11 +1,7 @@
+from hysut.utils.enums import RUN_PERIOD, WARM_PERIOD, COOL_PERIOD
 
-from hysut.utils.enums import (
-    RUN_PERIOD,
-    WARM_PERIOD,
-    COOL_PERIOD
-)
 
-def read_time_data(time_data,item):
+def read_time_data(time_data, item):
     """Reading the time_data for specific item and checking the possible errors
 
     Parameters
@@ -29,49 +25,106 @@ def read_time_data(time_data,item):
     time_collection = []
     errors = []
 
-    if not isinstance(time_data,list):
-        return {"error" : f"time should be defined as a list for '{item}'."}
+    if not isinstance(time_data, list):
+        return {"error": f"time should be defined as a list for '{item}'."}
 
     for time in time_data:
 
-        if isinstance(time,int):
+        if isinstance(time, int):
             time_collection.append(time)
 
-        elif isinstance(time,list):
-            if not all([isinstance(i,int) for  i in time]):
-                errors.append(f"time definition through lists can only contain integers for '{item}'.")
+        elif isinstance(time, list):
+            if not all([isinstance(i, int) for i in time]):
+                errors.append(
+                    f"time definition through lists can only contain integers for '{item}'."
+                )
             else:
                 time_collection.extend(time)
 
-        elif isinstance(time,str) and 'range' in time:
+        elif isinstance(time, str) and "range" in time:
             try:
                 time_collection.extend(list(eval(time)))
             except Exception as error:
                 errors.extend([f"{arg} in 'range' for '{item}'." for arg in error.args])
         else:
-            errors.append(f"time definition can be a range (e.g. range(start,end,step)),an integer or a list of integers for '{item}'")
+            errors.append(
+                f"time definition can be a range (e.g. range(start,end,step)),an integer or a list of integers for '{item}'"
+            )
 
-    return {
-        "time":time_collection,
-        "error": errors if len(errors) else None
-        }
-
-
-# def check_time_horizon(time_horizon):
-#     warnings = []
-#     errors   = []
-#     time_data = {}
-
-#     if RUN_PERIOD not in time_horizon:
-#         errors.append(
-#             f"A model cannot be created without a {RUN_PERIOD}."
-#         )
-
-#     for period in [RUN_PERIOD,WARM_PERIOD,COOL_PERIOD]:
-#         if period in time_horizon:
-#             given_period = time_horizon[period]
-#             data = read_time_data(given_period)
+    return {"time": time_collection, "error": errors if len(errors) else None}
 
 
-#     return
+def check_time_period_overlaps(periods):
+    """Checks if any overlap in the periods exist
+
+    Parameters
+    ----------
+    periods : dict
+        dict containing the time_data for different periods (values are sorted time horizons)
+
+    Returns
+    -------
+    list
+        a list of errors
+    """
+
+    errors = []
+    periods = {
+        period: list(range(sorted_list[0], sorted_list[-1] + 1))
+        for period, sorted_list in periods.items()
+    }
+    # Checking if duplicate values exist in different periods with run
+    for period in [WARM_PERIOD, COOL_PERIOD]:
+        if period in periods:
+            intersects = set(periods[RUN_PERIOD]).intersection(periods[period])
+            if intersects:
+                errors.append(
+                    f"time_horizon for 'run' and '{period}' have following intersections."
+                    f" \n{intersects}"
+                )
+
+    # checking if warm and cool period have intersects
+    if all([period in periods for period in [WARM_PERIOD, COOL_PERIOD]]):
+        intersects = set(periods[WARM_PERIOD]).intersection(periods[COOL_PERIOD])
+
+        if intersects:
+            errors.append(
+                f"time_horizon for '{WARM_PERIOD}' and '{COOL_PERIOD}' have following intersections."
+                f" \n{intersects}"
+            )
+
+    return errors
+
+
+def check_time_horizon(time_horizon):
+    warnings = []
+    errors = []
+    time_data = {}
+
+    if RUN_PERIOD not in time_horizon:
+        errors.append(f"A model cannot be created without a {RUN_PERIOD}.")
+
+    for period in [RUN_PERIOD, WARM_PERIOD, COOL_PERIOD]:
+        if period in time_horizon:
+
+            given_period = time_horizon[period]
+            data = read_time_data(given_period, period)
+
+            if data.get("error") is not None:
+                errors.append(data.get("error"))
+
+            else:
+                # avoid deuplicate values and sort the years
+                time_data[period] = sorted(set(data.get("time")))
+
+        else:
+            warnings.append(
+                f"{period} is not a valid argument for for time_horizon definition and"
+                " is ignored."
+            )
+
+    # catching the errors related to definition of time
+    errors.append(check_time_period_overlaps(time_data))
+
+
 # %%
--- tests/test_preprocess.py
+++ tests/test_preprocess.py
@@ -5,7 +5,14 @@ import os
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), ".."))
 
 from hysut.preprocess.time import (
-    read_time_data
+    read_time_data,
+    check_time_period_overlaps,
+)
+
+from hysut.utils.enums import (
+   RUN_PERIOD,
+   COOL_PERIOD,
+   WARM_PERIOD
 )
 
 def test_read_time_data():
@@ -69,3 +76,55 @@ def test_read_time_data():
     assert out['time'] == list(range(2020,2026))
     assert out['error'] is None
 
+
+def test_check_time_period_overlaps():
+
+   # run and warm overlap
+   periods = {
+      RUN_PERIOD : [2020,2022,2023,2025],
+      WARM_PERIOD: [2024,2028]
+   }
+
+   expected_error = f"time_horizon for 'run' and '{WARM_PERIOD}' have following intersections. \n{set([2024,2025])}"
+   error = check_time_period_overlaps(periods)[0]
+
+   assert error == expected_error
+
+   # warm and cool overlap
+   periods = {
+      RUN_PERIOD : [2020,2022,2023,2025],
+      COOL_PERIOD: [2026,2027],
+      WARM_PERIOD: [2027,2028]
+   }
+
+   expected_error = f"time_horizon for '{WARM_PERIOD}' and '{COOL_PERIOD}' have following intersections. \n{set([2027])}"
+   error = check_time_period_overlaps(periods)[0]
+
+   assert error == expected_error
+
+   # run,cool, and warm overlap
+
+   periods = {
+      RUN_PERIOD : [2020,2022,2023,2025],
+      WARM_PERIOD: [2024,2027],
+      COOL_PERIOD: [2027,2028]
+   }
+
+   expected_error = [
+      f"time_horizon for 'run' and '{WARM_PERIOD}' have following intersections. \n{set([2024,2025])}",
+      f"time_horizon for '{WARM_PERIOD}' and '{COOL_PERIOD}' have following intersections. \n{set([2027])}"
+   ]
+
+   error = check_time_period_overlaps(periods)
+
+   assert error == expected_error
+
+   # no error
+   periods = {
+      RUN_PERIOD : [2020,2022,2023,2025],
+      WARM_PERIOD: [2026,2027],
+      COOL_PERIOD: [2028,2030]
+   }
+
+   error = check_time_period_overlaps(periods)
+   assert error == []
