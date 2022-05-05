856a64c5545166ce17c588dfe299b6800c60e64f
Mohammad Amin Tahavori <50955527+mohammadamint@users.noreply.github.com>
2022-05-05 12:31:56 +0200

    Adding the defualt modules and updating enums
    - default module will collect a set of classes for different sets/items
      containing the defualt values of different properties. For example,
    all the defualt values for Time definiton can be collected in a class as
    follow:
    
    class Time:
    	Property_1 = "Defualt value 1"
    	Property_2 = "Default value 2"
    
    In case the default value for a given property (e.g. Property_1) is
    needed:
    
    default = Time.Property_1
    
    can be used.
    
    - updating enums with time_slices enums.

---
+++ hysut/utils/defaults.py
@@ -0,0 +1,5 @@
+
+class Time:
+
+    SLICE_NAME = 'time_slice'
+    T_SLICE = [1]
--- hysut/utils/enums.py
+++ hysut/utils/enums.py
@@ -2,3 +2,6 @@
 RUN_PERIOD = 'run'
 WARM_PERIOD = 'warm_up'
 COOL_PERIOD = "cool_down"
+
+T_SLICE = "slices"
+SLICE_NAME = "name"
