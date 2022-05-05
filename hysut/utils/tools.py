c753cec2150bae3d6aaba323e1d59551ab7b76b7
Mohammad Amin Tahavori <50955527+mohammadamint@users.noreply.github.com>
2022-05-05 12:42:26 +0200

    Creating the tools.py module for collecting utils tools
    - type_consistency_check:
    	checks if all the items of a given list have the same data type.
    if the data types are consistent it returns an empty list otherwise
    returns an error in a list so can be extended to global error list
    
    - read_range_function:
    	translates the range function as a string to a list. if any
    exception happens in doing so, the exceptions will be collected and
    returned to errors list

---
+++ hysut/utils/tools.py
@@ -0,0 +1,46 @@
+def type_consistency_check(data_list,item=None):
+    """Checks if all the item in a give list have uniform data type
+
+    Parameters
+    ----------
+    data_list : list
+        list of data
+    item : str
+        specific item which the check is performed (for better error message)
+
+    Returns
+    -------
+    list
+        list of errors if any
+    """
+
+    if not all([type(item) == type(data_list[0]) for item in data_list]):
+        return [f"'{item}' is not allowed to have different data type."]
+
+    return []
+
+
+def read_range_function(range_data,item):
+    """Reads the range function in data and create a list for the given range
+
+    Parameters
+    ----------
+    range_data : str
+        str resembling the range function
+    item : str
+        the given item that range function is given for (for better error definition)
+
+    Returns
+    -------
+    dict
+        retuning the dat and errors in a dict
+    """
+    data = []
+    errors = []
+
+    try:
+        data = list(eval(range_data))
+    except Exception as error:
+        errors = [f"{arg} in 'range' for '{item}'." for arg in error.args]
+
+    return {'data':data,"error":errors}
