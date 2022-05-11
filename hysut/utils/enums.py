8c39d3c27bd6aac0e77e132a91f6784fdbdfcbc9
Mohammad Amin Tahavori <50955527+mohammadamint@users.noreply.github.com>
2022-05-11 15:22:01 +0200

    updating enums

--- hysut/utils/enums.py
+++ hysut/utils/enums.py
@@ -1,6 +1,11 @@
+TIME_HORIZON = "time_horizon"
+
 RUN_PERIOD = "run"
 WARM_PERIOD = "warm_up"
 COOL_PERIOD = "cool_down"
+ALL_PERIOD = 'all_years'
 
 T_SLICE = "slices"
 SLICE_NAME = "name"
+
+SETTINGS = "settings"
