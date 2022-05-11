4c314cf6ec08b189a1bf2d6c4496439c8b1e61d2
Mohammad Amin Tahavori <50955527+mohammadamint@users.noreply.github.com>
2022-05-11 15:18:57 +0200

    Updating the exceptions
    - adding TimeHorizonError that will be raised if any error exists in the
      definiton of time throug ModelDataBase class

--- hysut/exceptions_logging/exceptions.py
+++ hysut/exceptions_logging/exceptions.py
@@ -1,3 +1,7 @@
 class EssentialSetMissing(Exception):
     """Raises when essential set for building model is missed.
     """
+
+
+class TimeHorizonError(Exception):
+    """Raises when there are errors in the definition of time_horizon"""
