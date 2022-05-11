ce0401a2b614c31ac086ad8681ceafb9ac5da98a
Mohammad Amin Tahavori <50955527+mohammadamint@users.noreply.github.com>
2022-05-11 15:20:24 +0200

    initial commit for database.py
    - Contains the ModelDataBase class that is the main class for reading
      the model_config and store the model reference energy system/ database
    - Functionalities will be tested throug tests/test_database.py

---
+++ hysut/preprocess/database.py
@@ -0,0 +1,52 @@
+import pandas as pd
+from yaml import warnings
+
+from hysut.preprocess.time import check_time_horizon
+from hysut.utils.enums import TIME_HORIZON, SETTINGS
+from hysut.exceptions_logging.exceptions import TimeHorizonError
+from hysut.utils.defaults import ModelSettings
+from hysut.utils.tools import print_log
+from copy import deepcopy
+
+
+class ModelDataBase:
+    def __init__(self, model_config):
+        self.warnings = []
+        self.model_config = deepcopy(model_config)
+
+    def _extract_time_horizon_data(self):
+        time = check_time_horizon(self.model_config[TIME_HORIZON])
+        errors = time["errors"]
+        warnings.extend(time['warnings'])
+        if errors:
+            save_directory = self.model_config[SETTINGS]["log_path"]
+            print_log(logs=errors, save_file=save_directory + "/error_log.txt")
+            raise TimeHorizonError(
+                f"{len(errors)} exists in the definition of {TIME_HORIZON}. The errors are listed in the error_log file located at {save_directory}"
+            )
+
+        self.years = time['data']
+
+
+
+    def _check_model_settings(self):
+
+        default_settings = ModelSettings()
+        settings = self.model_config.setdefault(SETTINGS, {})
+
+        for option in default_settings.KEYS:
+            item_set = settings.setdefault(
+                option, getattr(default_settings, option)
+            )
+
+            validation = getattr(default_settings,'validate_'+option)(item_set)
+            self.warnings.extend(validation['warning'])
+            settings[option] = validation['value']
+
+        # check extra keys
+        differences = set(settings).difference(default_settings.KEYS)
+        if differences:
+            self.warnings.append(f'Following keys in the model {SETTINGS} are not valid and are ignored: \n{differences}')
+
+        self.model_config[SETTINGS] = settings
+
---
+++ tests/test_database.py
@@ -0,0 +1,25 @@
+import sys
+import os
+import pytest
+
+from hysut.utils.enums import SETTINGS
+
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), ".."))
+
+from hysut.preprocess.database import ModelDataBase
+from hysut.utils.defaults import ModelSettings
+
+
+def test_model_settings():
+    settings = ModelSettings()
+
+    # no settings passed
+    test = ModelDataBase({})
+    test._check_model_settings()
+
+    for item in settings.KEYS:
+        assert test.model_config[SETTINGS][item] == getattr(settings,item)
+
+
+
+
